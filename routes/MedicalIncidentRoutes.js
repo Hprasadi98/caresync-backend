c37be3812dda945277c33d931e95f08e40152f5f
Sanduni Lakshika <110313255+slakshika2026@users.noreply.github.com>
2024-05-17 18:23:50 +0530

    feat:

--- routes/MedicalIncidentRoutes.js
+++ routes/MedicalIncidentRoutes.js
@@ -1,3 +1,25 @@
+// const express = require("express");
+// const router = express.Router();
+// const {
+//   createTestMedicalIncident,
+//   GetTestMedicalIncident,
+// } = require("../controllers/medicalIncidentController");
+
+
+
+// // //post a new test
+// router.post("/", createTestMedicalIncident);
+
+// //Get a test
+// router.get("/", GetTestMedicalIncident);
+
+
+
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const router = express.Router();
 const {
@@ -5,15 +27,25 @@ const {
   GetTestMedicalIncident,
 } = require("../controllers/medicalIncidentController");
 
- 
+const {
+  createSymptomMedicalIncident,
+} = require("../controllers/medicalIncidentSymptomController");
 
-// //post a new test
-router.post("/", createTestMedicalIncident);
+// Define a single POST route
+router.post("/", (req, res, next) => {
+  // Check condition based on request parameter or body property
+  const condition = req.body.type === "symptom"; // Example condition based on request body
 
-//Get a test
+  if (condition) {
+    // If condition is true, call the first controller function
+    createSymptomMedicalIncident(req, res, next);
+  } else {
+    // If condition is false, call the second controller function
+    createTestMedicalIncident(req, res, next);
+  }
+});
+
+// Get a test
 router.get("/", GetTestMedicalIncident);
-
-
-
 
 module.exports = router;
